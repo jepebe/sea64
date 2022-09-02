61caf7cf13521cb911da416d8e5ba42de2ee1bc5
Jean-Paul Balabanian <jepebe@users.noreply.github.com>
2022-09-02 16:28:20 +0200

    Add stack to debug info

--- include/debug.h
+++ include/debug.h
@@ -4,4 +4,6 @@ NO_RETURN void cpu_error(Machine *machine, char *message, ...);
 
 void cpu_error_marker(Machine *machine, char *file, int line);
 
+void cpu_stack_contents(Machine *machine);
+
 void cpu_instruction_context(Machine *machine);
--- src/debug.c
+++ src/debug.c
@@ -15,11 +15,17 @@ void cpu_instruction_context(Machine *machine) {
     }
     printf("\x1b[0m\n");
 
-    for(int seg = 0; seg < 3; ++seg) {
+    for (int seg = 0; seg < 3; ++seg) {
         printf("\x1b[0;31m[$%04X] ", segment);
         for (int i = 0; i < 16; ++i) {
             u32 addr = segment + i;
+            if (addr == machine->cpu.PC) {
+                printf("\x1b[1;91m");
+            }
             printf("%02X ", machine->ram[addr]);
+            if (addr == machine->cpu.PC) {
+                printf("\x1b[0;31m");
+            }
         }
         printf("\x1b[0m\n");
         segment += 0x10;
@@ -29,17 +35,50 @@ void cpu_instruction_context(Machine *machine) {
 NO_RETURN void cpu_error(Machine *machine, char *message, ...) {
     va_list argp;
     printf("\x1b[0;31m");
+
     printf("[$%04X] Error: ", machine->cpu.PC);
     va_start(argp, message);
     vprintf(message, argp);
     va_end(argp);
     printf("\x1b[0m\n");
+
     cpu_instruction_context(machine);
+
+    printf("\n");
+    cpu_stack_contents(machine);
+
+    printf("\x1b[0;31mFailure after %llu ticks.", machine->cpu.tick_count);
+    printf("\x1b[0m\n");
     exit(1);
 }
 
 void cpu_error_marker(Machine *machine, char *file, int line) {
     printf("\x1b[0;33m");
     printf("[$%04X] Note: %s:%d", machine->cpu.PC, file, line);
+    printf("\x1b[0m\n");
+}
+
+void cpu_stack_contents(Machine *machine) {
+    printf("\x1b[0;31m");
+    printf("[stack]\n");
+    u8 sp = 0xFF;
+    for (int row = 0; row < 16; ++row) {
+        printf("[$%04X] ", 0x100 + sp);
+        for (int col = 0; col < 16; ++col) {
+            if (sp == machine->cpu.S) {
+                printf("\x1b[1;91m");
+            }
+            printf("%02X ", machine->ram[0x100 + sp]);
+            if (sp == machine->cpu.S) {
+                printf("\x1b[0;31m");
+            }
+            sp--;
+        }
+        printf("\n");
+        if (sp < machine->cpu.S) {
+            break;
+        }
+    }
+
     printf("\x1b[0m\n");
 }
